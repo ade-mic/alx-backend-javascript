9041b438fe75eee04c9b059689edfd688c512f18
ade-mic <ademic.aina@gmail.com>
2024-10-07 17:53:10 +0100

    1-block-scoped.js

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-  const task = false;
-  const task2 = true;
-
-  if (trueOrFalse) {
-    task;
-    task2;
-   }
-
-   return [task, task2];
-}
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      // Perform operations or return task/task2 if needed
+    }
+  
+    return [task, task2];
+  }
+  
