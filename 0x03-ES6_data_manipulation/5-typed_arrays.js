119b4e75f97aa6c23a82ff8a19d47c114c30a063
ade-mic <ademic.aina@gmail.com>
2024-10-08 21:25:04 +0100

    5-typed_arrays.js

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,11 +1,11 @@
 export default function createInt8TypedArray(length, position, value) {
   const buffer = new ArrayBuffer(length);
-  const int8Array = new Int8Array(buffer);
+  const view = new DataView(buffer);
 
   if (position > length || position < 0) {
     throw new Error('Position outside range');
   }
 
-  int8Array[position] = value;
-  return buffer;
+  view.setInt8(position, value);
+  return view;
 }
