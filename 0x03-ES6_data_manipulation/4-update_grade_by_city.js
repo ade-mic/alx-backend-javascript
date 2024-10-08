3778e9fa318957d842628d4a9ae8c6400f310cc3
ade-mic <ademic.aina@gmail.com>
2024-10-08 19:29:58 +0100

    4-update_grade_by_city.js

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,9 @@
+export default function updateStudentGradeByCity(studentList, city, newGrades) {
+  return studentList.filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      return {
+        ...student, grade: gradeObj ? gradeObj.grade : 'N/A',
+      };
+    });
+}
