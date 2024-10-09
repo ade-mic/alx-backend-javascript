5602ce4bd3c606770e227e4acd92940e31886678
ade-mic <ademic.aina@gmail.com>
2024-10-09 17:35:43 +0100

    8-clean_set.js

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (!startString || typeof startString !== 'string') {
-    return '';
-  }
-
   let result = '';
   set.forEach((value) => {
     if (value.startsWith(startString)) {
