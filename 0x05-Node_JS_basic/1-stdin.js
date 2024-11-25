042e3b2dd09f05880200ad46466237de24cdd006
ade-mic <ademic.aina@gmail.com>
2024-11-25 18:42:37 +0100

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -7,9 +7,9 @@ process.stdin.setEncoding('utf-8');
 process.stdin.on('data', (input) => {
   const name = input.trim();
   console.log(`Your name is: ${name}`);
+  process.stdin.end();
 });
 
 process.stdin.on('end', () => {
   console.log('This important software is now closing');
-  process.exit();
 });
