47fc2ea16bdc9a7e6f21a2acdeb93d19a794c8c7
ade-mic <ademic.aina@gmail.com>
2024-11-25 18:54:45 +0100

    1-stdin.js

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,6 +1,6 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.on('data', (input) => {
+process.stdin.on('input', (input) => {
   const name = input.toString().trim();
   process.stdout.write(`Your name is: ${name}\n`);
   process.stdin.end();
