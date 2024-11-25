17571a185eba7ad460e87c69d508b4910e2a730c
ade-mic <ademic.aina@gmail.com>
2024-11-25 18:50:56 +0100

    1. Using Process stdin

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,14 @@
 // display message
-console.log('Welcome to Holberton School, what is your name?');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-// input your name
-process.stdin.setEncoding('utf-8');
+
 
 process.stdin.on('data', (input) => {
   const name = input.toString().trim();
-  console.log(`Your name is: ${name}`);
+  process.stdout.write(`Your name is: ${name}\n`);
   process.stdin.end();
 });
 
 process.stdin.on('end', () => {
-  console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
