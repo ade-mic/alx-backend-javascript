63b9805cefa19540429819c5274aa327c5cf5c1f
ade-mic <ademic.aina@gmail.com>
2024-11-26 20:31:20 +0100

    nodes

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,41 +1,39 @@
 import fs from 'fs';
 
-const readDatabase = (filePath) => {
-  return new Promise((resolve, reject) => {
-    // Read the file asynchronously
-    fs.readFile(filePath, 'utf-8', (err, data) => {
-      if (err) {
-        // Reject the promise if the file cannot be read
-        reject(err);
-        return;
+const readDatabase = (filePath) => new Promise((resolve, reject) => {
+  // Read the file asynchronously
+  fs.readFile(filePath, 'utf-8', (err, data) => {
+    if (err) {
+      // Reject the promise if the file cannot be read
+      reject(err);
+      return;
+    }
+
+    // Parse the file contents
+    const studentsByField = {};
+    const lines = data.trim().split('\n');
+    const headers = lines[0].split(',');
+
+    // Check if headers are as expected
+    const expectedHeaders = ['firstname', 'lastname', 'age', 'field'];
+    if (!headers.every((header, index) => header.trim() === expectedHeaders[index])) {
+      reject(new Error('Invalid database format'));
+      return;
+    }
+
+    // Process each line (skipping the header line)
+    for (let i = 1; i < lines.length; i += 1) {
+      const [firstname, , , field] = lines[i].split(',');
+
+      if (!studentsByField[field]) {
+        studentsByField[field] = [];
       }
+      studentsByField[field].push(firstname);
+    }
 
-      // Parse the file contents
-      const studentsByField = {};
-      const lines = data.trim().split('\n');
-      const headers = lines[0].split(',');
-
-      // Check if headers are as expected
-      const expectedHeaders = ['firstname', 'lastname', 'age', 'field'];
-      if (!headers.every((header, index) => header.trim() === expectedHeaders[index])) {
-        reject(new Error('Invalid database format'));
-        return;
-      }
-
-      // Process each line (skipping the header line)
-      for (let i = 1; i < lines.length; i++) {
-        const [firstname, , , field] = lines[i].split(',');
-
-        if (!studentsByField[field]) {
-          studentsByField[field] = [];
-        }
-        studentsByField[field].push(firstname);
-      }
-
-      // Resolve the promise with the resulting object
-      resolve(studentsByField);
-    });
+    // Resolve the promise with the resulting object
+    resolve(studentsByField);
   });
-};
+});
 
 export default readDatabase;
