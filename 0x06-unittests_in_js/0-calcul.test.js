1784dcbeebaced31851c5589ce525361643906c7
ade-mic <ademic.aina@gmail.com>
2024-11-28 05:46:16 +0100

    3. Spies

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -1,20 +1,41 @@
 const assert = require('assert');
 const calculateNumber = require('./0-calcul');
 
-describe('calculateNumber', function() {
-  it('should return 4 when inputs are 1 and 3', function() {
-    assert.strictEqual(calculateNumber(1, 3), 4);
-  });
-
-  it('should return 5 when inputs are 1 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1, 3.7), 5);
-  });
-
-  it('should return 5 when inputs are 1.2 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
-  });
-
-  it('should return 6 when inputs are 1.5 and 3.7', function() {
-    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
-  });
-});
+describe('calculateNumber', () => {
+  it('should return rounded addition of 2.4, 3.9', () => {
+    assert.strictEqual(calculateNumber(2.4, 3.9), 6)
+  })
+  it('calculateNumber(1, 3)', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4)
+  })
+  it('calculateNumber(1, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1, 3.7), 5)
+  })
+  it ('calculateNumber(1.5, 3.7)', () => {
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6)
+  })
+  it('calculateNumber(0.5, -7)', () => {
+    assert.strictEqual(calculateNumber(0.5, -7), -6)
+  })
+  it('calculateNumber(-1, -1)', () => {
+    assert.strictEqual(calculateNumber(-1, 1), 0)
+  })
+  it('calculateNumber(0, 0)', () => {
+    assert.strictEqual(calculateNumber(0, 0), 0)
+  })
+  it('calculateNumber(0, "a")', () => {
+    assert.strictEqual(calculateNumber(0, 'a'), NaN)
+  })
+  it('calculateNumber(1.5, "1")', () => {
+    assert.strictEqual(calculateNumber(1.5, '1'), 3)
+  })
+  it('calculateNumber("-7", "1")', () => {
+    assert.strictEqual(calculateNumber('-7', '1'), -6)
+  })
+  it('calculateNumber("-5.6", "1000")', () => {
+    assert.strictEqual(calculateNumber('-5.6', '1000'), 994)
+  })
+  it('calculateNumber("-5000", "-111111")', () => {
+    assert.strictEqual(calculateNumber('-5000', '-111111'), -116111)
+  })
+})
---
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -0,0 +1,13 @@
+const sinon = require('sinon');
+const assert = require('assert');
+const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+
+describe('sendPaymentRequestToApi', function() {
+  it('should call Utils.calculateNumber with SUM, 100, and 20', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    sendPaymentRequestToApi(100, 20);
+    assert(spy.calledOnceWithExactly('SUM', 100, 20));
+    spy.restore();
+  });
+});
