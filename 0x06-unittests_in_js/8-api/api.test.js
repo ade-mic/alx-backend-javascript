fcaabdba4d66bd34f7426b1fc91c21ac6761f622
ade-mic <ademic.aina@gmail.com>
2024-11-28 08:44:36 +0100

    test 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,32 +1,20 @@
+process.noDeprecation = true;
 const request = require('request');
 const { expect } = require('chai');
 
 describe('Index page', () => {
-  const baseUrl = 'http://localhost:7865';
+  const url = 'http://localhost:7865/';
 
-  // Before starting tests, ensure the server is running
-  before((done) => {
-    require('./api'); // Start the server
-    done();
-  });
-
-  it('should return the correct status code', (done) => {
-    request.get(baseUrl, (err, res) => {
-      expect(res.statusCode).to.equal(200);
+  it('should return status code 200', (done) => {
+    request(url, (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
       done();
     });
   });
 
   it('should return the correct message', (done) => {
-    request.get(baseUrl, (err, res) => {
-      expect(res.body).to.equal('Welcome to the payment system');
-      done();
-    });
-  });
-
-  it('should return a plain text content-type', (done) => {
-    request.get(baseUrl, (err, res) => {
-      expect(res.headers['content-type']).to.include('text/html');
+    request(url, (error, response, body) => {
+      expect(body).to.equal('Welcome to the payment system');
       done();
     });
   });
