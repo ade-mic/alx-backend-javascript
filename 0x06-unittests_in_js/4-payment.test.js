422cfff645cede28f34dcca085988dd5c0ba349e
ade-mic <ademic.aina@gmail.com>
2024-11-28 06:59:13 +0100

    4-payment.test.js

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,28 +1,39 @@
 const sinon = require('sinon');
-const assert = require('assert');
+const { expect } = require('chai');
 const Utils = require('./utils');
 const sendPaymentRequestToApi = require('./4-payment');
 
-describe('sendPaymentRequestToApi', function() {
+describe('sendPaymentRequestToApi', function () {
+  let calculateNumberStub, consoleLogSpy;
 
+  beforeEach(function () {
+    // Stub Utils.calculateNumber
+    calculateNumberStub = sinon.stub(Utils, 'calculateNumber');
+    calculateNumberStub.returns(10);
 
-  this.beforeEach(function() {
-    const stub = sinon.stub(Utils, 'calculateNumber').returns(10);
-    const spy = sinon.spy(console, 'log');
+    // Spy on console.log
+    consoleLogSpy = sinon.spy(console, 'log');
   });
 
-  this.afterEach(function() {
-    stub.restore();
-    spy.restore();
-  })
-
-  it('should call Utils.calculateNumber with SUM, 100, and 20', function() {
-    sendPaymentRequestToApi(100, 20);
-    assert(stub.calledOnceWithExactly('SUM', 100, 20));
+  afterEach(function () {
+    // Restore the stub and spy
+    calculateNumberStub.restore();
+    consoleLogSpy.restore();
   });
 
-  it('should log the message', function() {
+  it('should call Utils.calculateNumber with type = SUM, a = 100, b = 20', function () {
     sendPaymentRequestToApi(100, 20);
-    assert(spy.calledOnceWithExactly('The total is: 10'));
-  })
+
+    // Verify the stub was called correctly
+    expect(calculateNumberStub.calledOnce).to.be.true;
+    expect(calculateNumberStub.calledWith('SUM', 100, 20)).to.be.true;
+  });
+
+  it('should log "The total is: 10"', function () {
+    sendPaymentRequestToApi(100, 20);
+
+    // Verify console.log was called correctly
+    expect(consoleLogSpy.calledOnce).to.be.true;
+    expect(consoleLogSpy.calledWith('The total is: 10')).to.be.true;
+  });
 });
