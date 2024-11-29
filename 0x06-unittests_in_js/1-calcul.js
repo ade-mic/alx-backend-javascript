b4a1166d520fb6395caf7d097612b3dc6f6b13e4
ade-mic <ademic.aina@gmail.com>
2024-11-29 20:57:37 +0100

    1-calcul.test.js

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -4,7 +4,7 @@ function calculateNumber(type, a, b) {
   } else if (type === 'SUBTRACT') {
     return (Math.round(a) - Math.round(b))
   } else if (type == 'DIVIDE') {
-    if (b == 0) {
+    if (b == 0 || Math.round(b) == 0) {
       return 'Error'
     }
     return (Math.round(a) / Math.round(b))
