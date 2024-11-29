be57a7b553680a7033a1ebcf76ca5bd988c89b72
ade-mic <ademic.aina@gmail.com>
2024-11-29 22:17:20 +0100

    correct req/

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,28 +1,35 @@
-const request = require('request');
-const { expect } = require('chai');
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const { expect } = chai;
+
+chai.use(chaiHttp);
 
 describe('Index page', () => {
-  const baseUrl = 'http://localhost:7865';
-
   it('Correct status code?', (done) => {
-    request.get(baseUrl, (err, res, body) => {
-      expect(res.statusCode).to.equal(200);
-      done();
-    });
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 
   it('Correct result?', (done) => {
-    request.get(baseUrl, { json: true }, (err, res, body) => {
-      expect(body).to.be.an('object');
-      expect(body.message).to.equal('Welcome to the payment system');
-      done();
-    });
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        expect(res.body).to.be.an('object');
+        expect(res.body.message).to.equal('Welcome to the payment system');
+        done();
+      });
   });
 
   it('Other?', (done) => {
-    request.get(baseUrl, (err, res, body) => {
-      expect(res.headers['content-type']).to.include('application/json');
-      done();
-    });
+    chai.request('http://localhost:7865')
+      .get('/')
+      .end((err, res) => {
+        expect(res.headers['content-type']).to.include('application/json');
+        done();
+      });
   });
 });
