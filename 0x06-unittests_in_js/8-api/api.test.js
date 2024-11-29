61f7865bedd61e40dc906d8dc4031ebae89e88ed
ade-mic <ademic.aina@gmail.com>
2024-11-29 21:29:07 +0100

    package.json

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -19,3 +19,15 @@ describe('Index page', () => {
     });
   });
 });
+
+describe('Index page', () => {
+  const endpoint = 'http://localhost:7865';
+
+  it('Returns the right status', function (done) {
+    request(endpoint, function (error, response, body) {
+      expect(response.statusCode).to.equal(200);
+      done();
+    });
+  });
+});
+
