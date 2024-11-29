57d8c11c9a7eec90ca13a953660b5609202d43bb
ade-mic <ademic.aina@gmail.com>
2024-11-29 22:27:37 +0100

    add status code

--- 0x06-unittests_in_js/8-api/api.js
+++ 0x06-unittests_in_js/8-api/api.js
@@ -2,12 +2,11 @@ const express = require('express');
 
 const app = express();
 
-// Define the root route
+
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to the payment system' });
 });
 
-// Start the server
 const PORT = 7865;
 app.listen(PORT, () => {
   console.log(`API available on localhost port ${PORT}`);
